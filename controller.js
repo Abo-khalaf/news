d177e62fee333505e9a3512a20bce5bb92e62c1a
muhamed kheer abo khalaf <moody.kh.022@gmail.com>
2018-12-14 14:40:19 +0100

    second-commit

--- controller.js
+++ controller.js
@@ -12,5 +12,15 @@ app.controller ('addNewsCtrl',['$scope','getNews', function($scope,getNews){
      }); 
     }
 
+    $scope.click = function() {
+        getNews.inputSE()
+        .then(function(adata){
+            console.log(adata.data.articles);
+   //console.log(Object.values(adata.data));
+           $scope.SE=adata.data.articles;
+           $scope.newsDescription=adata.data.articles.description;
+           $scope.image=adata.data.articles.urlToImage;
+        }); 
+       }
  }]);
  
--- serveses.js
+++ serveses.js
@@ -4,6 +4,14 @@ app.factory('getNews',['$http',function($http){
     var news = {
         inputNews:function(){
    
+         var url = 'https://newsapi.org/v2/top-headlines?sources=al-jazeera-english&apiKey=030f2481a2df4fecb04bd2f406f6ce8c';
+            return $http.get(url)
+            .then (function(adata){
+                return adata
+            })
+        },
+        inputSE:function(){
+   
          var url = 'https://newsapi.org/v2/top-headlines?country=se&apiKey=030f2481a2df4fecb04bd2f406f6ce8c';
             return $http.get(url)
             .then (function(adata){
